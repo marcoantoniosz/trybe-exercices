c4e96cff936e1547c1267ffe3610da37ff5b9c0f
Marco <marcoantonio99d@gmail.com>
2021-06-23 18:38:03 -0300

    exercicio 3 resolvido

---
+++ bloco_4/introducao-a-logica-e-programacao-1/exercicio_3.js
@@ -0,0 +1,10 @@
+let numero1 = 521;
+let numero2 = 631;
+let numero3 = 800;
+if (numero1 > numero2 && numero1 > numero3) {
+    console.log(numero1);
+} else if (numero2 > numero1 && numero2 > numero3) {
+    console.log(numero2);
+} else {
+    console.log(numero3);
+};
