7a0e7c449f4960f12280045923b2ce14516d39cc
Marco <marcoantonio99d@gmail.com>
2021-06-26 18:12:28 -0300

    inicia exercicio 11

--- bloco_4/introducao-a-logica-e-programacao-1/exercicio_10.js
+++ bloco_4/introducao-a-logica-e-programacao-1/exercicio_10.js
@@ -1,10 +1,10 @@
-let custoProduto = 1;
-let valorVenda = 5;
+let custoProduto = 10;
+let valorVenda = 30;
 let custoTotalProduto = (custoProduto) * (1.2);
+let lucro = (valorVenda - custoTotalProduto);
 
 if (custoProduto < 0 || valorVenda < 0) {
     console.log("Número invalido - negativo");
 } else if (custoProduto > 0 && valorVenda > 0) {
-    let lucro = (valorVenda - custoTotalProduto) * (1000);
-    console.log(lucro)
+    console.log(lucro * 1000);
 };
