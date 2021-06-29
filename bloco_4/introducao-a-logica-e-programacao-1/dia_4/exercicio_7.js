84f892426282f400a692953adbe388a19491c789
Marco <marcoantonio99d@gmail.com>
2021-06-28 22:57:55 -0300

    termina exercicio 7

---
+++ bloco_4/introducao-a-logica-e-programacao-1/dia_4/exercicio_7.js
@@ -0,0 +1,8 @@
+function somaNumeros(numero) {
+    let resultado = 0;
+    for (let index = 0; index <= numero; index += 1) {
+        resultado = resultado + index
+    } console.log(resultado)
+}
+
+somaNumeros(20)
