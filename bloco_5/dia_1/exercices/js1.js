3416c7616b3587841f9d67fa93f3527bef2e8f42
Marco <marcoantonio99d@gmail.com>
2021-07-05 18:53:31 -0300

    requisito 2 completo

--- bloco_5/dia_1/exercices/js1.js
+++ bloco_5/dia_1/exercices/js1.js
@@ -1,6 +1,8 @@
 const paragrafo = document.getElementsByTagName("p")[0];
-        paragrafo.innerText = "Daqui dois anos me vejo atuando como desenvolvedor web, cursando uma faculdade de analise e desenvolvimento de softwares em outra cidade.";
+  paragrafo.innerText = "Daqui dois anos me vejo atuando como desenvolvedor web, cursando uma faculdade de analise e desenvolvimento de softwares em outra cidade.";
 
+const quadro = document.getElementsByClassName("main-content")[0];
+	quadro.style.backgroundColor = "rgb(76,164,109)";
 
 
 
@@ -28,7 +30,6 @@ const paragrafo = document.getElementsByTagName("p")[0];
         - document.getElementById()
         - document.getElementsByClassName()
         - document.getElementsByTagName()
-        1. Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
         2. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
         3. Crie uma função que mude a cor do quadrado vermelho para branco.
         4. Crie uma função que corrija o texto da tag <h1>.
