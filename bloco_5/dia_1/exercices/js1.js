bd88e7f0ce49c8e985c6e76157edf5d56ea40515
Marco <marcoantonio99d@gmail.com>
2021-07-05 18:55:27 -0300

    requisito 2 completo

--- bloco_5/dia_1/exercices/js1.js
+++ bloco_5/dia_1/exercices/js1.js
@@ -1,10 +1,11 @@
 const paragrafo = document.getElementsByTagName("p")[0];
   paragrafo.innerText = "Daqui dois anos me vejo atuando como desenvolvedor web, cursando uma faculdade de analise e desenvolvimento de softwares em outra cidade.";
 
-const quadro = document.getElementsByClassName("main-content")[0];
-	quadro.style.backgroundColor = "rgb(76,164,109)";
+const quadroAm = document.getElementsByClassName("main-content")[0];
+	quadroAm.style.backgroundColor = "rgb(76,164,109)";
 
-
+const quadroVr = document.getElementsByClassName("center-content")[0];
+	quadroVr.style.backgroundColor = "white";
 
 
 
@@ -30,7 +31,6 @@ const quadro = document.getElementsByClassName("main-content")[0];
         - document.getElementById()
         - document.getElementsByClassName()
         - document.getElementsByTagName()
-        2. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
         3. Crie uma função que mude a cor do quadrado vermelho para branco.
         4. Crie uma função que corrija o texto da tag <h1>.
         5. Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
