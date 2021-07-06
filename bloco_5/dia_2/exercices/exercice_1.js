58a097dc69ee4550c5a1c53a7f5ef9e7aa721539
Marco <marcoantonio99d@gmail.com>
2021-07-05 22:54:35 -0300

    requisito 1 completo

---
+++ bloco_5/dia_2/exercices/exercice_1.js
@@ -0,0 +1,6 @@
+const newTitle = document.createElement('h1');
+const masterBody = document.getElementsByTagName('body')[0];
+newTitle.className = 'title';
+newTitle.innerText = 'Exercício 5.2 - JavaScript DOM';
+masterBody.appendChild(newTitle)
+
