0a1d536f679945cbdf0001341d5a76b1a2b8177c
Marco <marcoantonio99d@gmail.com>
2021-07-06 22:58:09 -0300

    requisito 1 completo

--- bloco_5/dia_3/exercices/script.js
+++ bloco_5/dia_3/exercices/script.js
@@ -13,4 +13,26 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
+function createDayz(){
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const listaMae = document.getElementById('days');
+
+  for(let i = 0; i < dezDaysList.length; i += 1) {
+    const diaAtual = dezDaysList[i];
+    const diaMes = document.createElement('li');
+    diaMes.innerText = diaAtual;
+
+    if (diaAtual === 25) {
+      diaMes.classList = 'day holiday friday';
+    } else if (diaAtual === 24 || diaAtual === 31) {
+      diaMes.classList = 'day holiday';
+    } else if (diaAtual === 4 || diaAtual === 11 || diaAtual === 18){
+      diaMes.classList = 'day friday';
+    } else {
+      diaMes.classList = 'day';
+    }
+    listaMae.appendChild(diaMes);
+  };
+};
+
+createDayz();
