1fb4f15b835287936725c4f6ab5500159bf481aa
Marco Souza <marcoantonio99d@gmail.com>
2021-11-22 19:22:45 -0300

    organiza pastas/2

--- bloco_8/dia_1/pratica.js
+++
@@ -1,7 +0,0 @@
-const wakeUp = () => 'Acordando!!';
-const breakfast = () => 'Bora tomar café!!';
-const goingToSleep = () => 'Partiu dormir!!';
-const doingThings = (func) => func();
-
-console.log(doingThings(breakfast));
-
