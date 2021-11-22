eb43f76b006a8049b5e236dc92b5d3b92fe7cd9d
Marco Souza <marcoantonio99d@gmail.com>
2021-11-22 19:11:17 -0300

    exercicio 3 resolvido

--- bloco_8/dia_1/exercicio_2.js
+++ bloco_8/dia_1/exercicio_2.js
@@ -1,5 +1,5 @@
 const sorteio = () => {
-    const numbers = [1, 2 , 3, 4, 5];
+    const numbers = [1, 2, 3, 4, 5];
     const sorteio = parseInt(Math.random() * 5);
     return numbers[sorteio];
 };
