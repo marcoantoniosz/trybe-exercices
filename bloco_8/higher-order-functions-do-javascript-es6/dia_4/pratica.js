f45f389bcc2875e278c6b1d0c1ef2079dedd3e94
Marco Souza <marcoantonio99d@gmail.com>
2021-11-23 16:00:59 -0300

    pratica concluida

---
+++ bloco_8/higher-order-functions-do-javascript-es6/dia_4/pratica.js
@@ -0,0 +1,105 @@
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+//1
+
+const oddNumbs = numbers.filter((number) => number % 2 === 0);
+const sum = oddNumbs.reduce((acc, curr) => acc + curr);
+
+//2
+
+const getOdd = (acc, curr) => ((curr % 2 === 0) ? acc + curr : acc);
+
+const sumn = (array) => array.reduce(getOdd, 0);
+
+console.log(sumn(numbers));
+
+//3
+
+const estudantes = [
+    {
+      nome: 'Jorge',
+      sobrenome: 'Silva',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 67 },
+        { name: 'Português', nota: 79 },
+        { name: 'Química', nota: 70 },
+        { name: 'Biologia', nota: 65 },
+      ],
+    },
+    {
+      nome: 'Mario',
+      sobrenome: 'Ferreira',
+      idade: 15,
+      turno: 'Tarde',
+      materias: [
+        { name: 'Matemática', nota: 59 },
+        { name: 'Português', nota: 80 },
+        { name: 'Química', nota: 78 },
+        { name: 'Biologia', nota: 92 },
+      ],
+    },
+    {
+      nome: 'Jorge',
+      sobrenome: 'Santos',
+      idade: 15,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 76 },
+        { name: 'Português', nota: 90 },
+        { name: 'Química', nota: 70 },
+        { name: 'Biologia', nota: 80 },
+      ],
+    },
+    {
+      nome: 'Maria',
+      sobrenome: 'Silveira',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 91 },
+        { name: 'Português', nota: 85 },
+        { name: 'Química', nota: 92 },
+        { name: 'Biologia', nota: 90 },
+      ],
+    },
+    {
+      nome: 'Natalia',
+      sobrenome: 'Castro',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 70 },
+        { name: 'Português', nota: 70 },
+        { name: 'Química', nota: 60 },
+        { name: 'Biologia', nota: 50 },
+      ],
+    },
+    {
+      nome: 'Wilson',
+      sobrenome: 'Martins',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 80 },
+        { name: 'Português', nota: 82 },
+        { name: 'Química', nota: 79 },
+        { name: 'Biologia', nota: 75 },
+      ],
+    },
+  ];
+
+const melhorNota = (acc, curr) => (acc.nota > curr.nota) ? curr : acc;
+    
+
+
+function newArray (arr) {
+    const newArr = arr.map((current) => ({
+        name: current.nome,
+        materia: current.materias.reduce(melhorNota).name,
+    }));
+    return newArr;
+}
+
+console.log(newArray(estudantes));
