8c64aa24bd6b419d3273aa88da12a1252f115ae8
Marco Souza <marcoantonio99d@gmail.com>
2021-11-22 21:27:54 -0300

    s

---
+++ bloco_4/introducao-a-logica-e-programacao-1/dia_1/exercicio_1.js
@@ -0,0 +1,8 @@
+let a = 1;
+let b = 2;
+let resultadoSoma = a + b;
+let resultadoSubtracao = a - b;
+let resultadoDivisao = a / b;
+let resultadoMultiplicacao = a * b;
+let resultadoModulo = a % b;
+console.log(resultadoDivisao, resultadoModulo, resultadoMultiplicacao, resultadoSoma, resultadoSubtracao);
